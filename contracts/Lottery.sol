81381344d098d971b76e9e5e4cbb59ad4b1a270e
Inallhonesty <inallhonesty92@gmail.com>
2023-04-27 12:51:53 +0300

    Improve config.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,19 +3,30 @@
 pragma solidity >=0.8 <0.9.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    // OPEN = 0, CLOSED = 1, CALCULATING_WINNER = 2
+
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) {
         usdEntryFee = 12 * (10 ** 18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
         // 12$ min
+        require(lottery_state == LOTTERY_STATE.OPEN);
         require(
             msg.value >= usdEntryFee,
             "Not enough ether to enter the lottery"
@@ -34,7 +45,16 @@ contract Lottery {
 
     //modifier onlyAdmin() {}
 
-    //function startLottery() public onlyAdmin {}
+    //onlyAdmin
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+    }
 }
